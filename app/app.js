1d5d07a74837382996092d08f433cf5647c4e5aa
Madanmohan Kannojiya <madanmohan.kannojiya@T-Mobile.com>
2016-06-23 12:03:52 +0530

    Added new functionality

--- app/app.js
+++ app/app.js
@@ -1,12 +1,18 @@
 angular.module('myTmoApp',['myTmoApp.loginModule','ui.bootstrap']);
-angular.module('myTmoApp.loginModule').config(function ($stateProvider ,$locationProvider, $urlRouterProvider) {
+angular.module('myTmoApp.loginModule').config(function ($stateProvider ,$locationProvider, $urlRouterProvider,LOGIN_CONST) {
   //$urlRouterProvider.otherwise("/");
   $locationProvider.html5Mode(true).hashPrefix('!');
   $stateProvider
       .state('logout', {
         url: '/logout',
-		template:'<h1>Got Logout</h1>'
+		template:'<h1>Welcome {{username}}</h1>',
+		controller: function($scope){
+			$scope.username = sessionStorage.getItem('username') ;
+		}
       });
+	  
+	LOGIN_CONST.LOGIN_URL = '/validateOtherUser';
+	LOGIN_CONST.NEXT_STATE = 'logout';
 console.log('stateProvider',$stateProvider.state);
   })
   
